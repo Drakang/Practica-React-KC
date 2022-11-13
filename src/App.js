2683c67a7af6bb620f7cb91dee2138060169d7ba
David Monroy <david2310.dmc@gmail.com>
2022-11-13 07:26:01 +0000

    Conexion con la api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom"
 
-function App() {
+import {
+  AdvertPage,
+  AdvertsPage,
+  AuthContext,
+  NewAdvertPage,
+  RequireAuth,
+} from "./components/auth"
+import LoginPage from "./components"
+import { useState } from "react"
+
+const App = ({ isPrevLogged }) => {
+  const [isLogged, setIsLogged] = useState(isPrevLogged)
+  // console.log("ta loguiado?: ", isLogged)
+  const handleLoginState = (value) => setIsLogged(value)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AuthContext value={{ isLogged, handleLoginState }}>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/" element={<Navigate to="/adverts" />} />
+          <Route
+            path="/adverts"
+            element={
+              <RequireAuth>
+                <AdvertsPage />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/adverts/:id"
+            element={
+              <RequireAuth>
+                <AdvertPage />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/adverts/new"
+            element={
+              <RequireAuth>
+                <NewAdvertPage />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="*"
+            element={
+              <main>
+                <h2>Oops!</h2>
+                <p>No se encontro nada</p>
+              </main>
+            }
+          />
+        </Routes>
+      </Router>
+    </AuthContext>
+  )
 }
 
-export default App;
+export default App
