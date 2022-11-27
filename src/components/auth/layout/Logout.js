7b8928b739ef5f32f56586bbe7f68bbb99071581
David Monroy <david2310.dmc@gmail.com>
2022-11-27 20:28:42 +0000

    Solucion logout

--- src/components/auth/layout/Logout.js
+++ src/components/auth/layout/Logout.js
@@ -1,41 +1,43 @@
-import { Button, Confirmation } from "../../commons"
-import { useState } from "react"
-import classNames from "classnames"
-import { logout } from "../dataService"
-import { useNavigate } from "react-router"
+import { Button, Confirmation } from "../../commons";
+import { useState } from "react";
+import classNames from "classnames";
+import { logout } from "../dataService";
+import { useNavigate } from "react-router";
 
-import { useContext } from "react"
-import AuthContext from "../context"
-import "./NavBar.css"
-import React from "react"
+import { useContext } from "react";
+import AuthContext from "../context";
+import "./NavBar.css";
+import React from "react";
 
 const Logout = () => {
-  const [showConfirm, setShowConfirm] = useState(false)
-  const navigate = useNavigate()
-  const {handleLoginState} = useContext(AuthContext)
+  const [showConfirm, setShowConfirm] = useState(false);
+  const navigate = useNavigate();
+  const { handleLoginState } = useContext(AuthContext);
 
   const handleClick = () => {
-    setShowConfirm(true)
-  }
+    setShowConfirm(true);
+  };
 
-  const onLogout = ()=>{
-    console.log('logout')
+  const onLogout = () => {
     logout()
-    handleLoginState(false)
-    navigate("/", { replace: true })
-  }
+      .then(handleLoginState(false))
+      .then(navigate("/", { replace: true }))
+      .catch((error) => {
+        console.log(error.status);
+      });
+  };
 
   return (
     <React.Fragment>
       <Button onClick={handleClick}>Logout</Button>
       <Confirmation
         className={classNames("logout-confirm", { hidden: !showConfirm })}
-        msg={'¿Cerrar sesión?'}
+        msg={"¿Cerrar sesión?"}
         showConfirm={setShowConfirm}
         onConfirm={onLogout}
       />
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default Logout
+export default Logout;
